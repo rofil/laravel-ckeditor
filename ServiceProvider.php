747c029ff5c79c5191d1ddd23be98b0ac3887a37
Rofilde Hasudungan <rofilde.hasudungan@gmail.com>
2016-01-12 12:12:20 +0800

    Modified the package information

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Unisharp\Ckeditor;
+namespace Rofilde\Ckeditor;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
@@ -9,14 +9,14 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->publishes(
             [
-                __DIR__ . '/ckeditor.js' => public_path('vendor/unisharp/laravel-ckeditor/ckeditor.js'),
-                __DIR__ . '/config.js' => public_path('vendor/unisharp/laravel-ckeditor/config.js'),
-                __DIR__ . '/styles.js' => public_path('vendor/unisharp/laravel-ckeditor/styles.js'),
-                __DIR__ . '/contents.css' => public_path('vendor/unisharp/laravel-ckeditor/contents.css'),
-                __DIR__ . '/adapters' => public_path('vendor/unisharp/laravel-ckeditor/adapters'),
-                __DIR__ . '/lang' => public_path('vendor/unisharp/laravel-ckeditor/lang'),
-                __DIR__ . '/skins' => public_path('vendor/unisharp/laravel-ckeditor/skins'),
-                __DIR__ . '/plugins' => public_path('vendor/unisharp/laravel-ckeditor/plugins'),
+                __DIR__ . '/ckeditor.js' => public_path('rofilde-ckeditor/laravel-ckeditor/ckeditor.js'),
+                __DIR__ . '/config.js' => public_path('rofilde-ckeditor/laravel-ckeditor/config.js'),
+                __DIR__ . '/styles.js' => public_path('rofilde-ckeditor/laravel-ckeditor/styles.js'),
+                __DIR__ . '/contents.css' => public_path('rofilde-ckeditor/laravel-ckeditor/contents.css'),
+                __DIR__ . '/adapters' => public_path('rofilde-ckeditor/laravel-ckeditor/adapters'),
+                __DIR__ . '/lang' => public_path('rofilde-ckeditor/laravel-ckeditor/lang'),
+                __DIR__ . '/skins' => public_path('rofilde-ckeditor/laravel-ckeditor/skins'),
+                __DIR__ . '/plugins' => public_path('rofilde-ckeditor/laravel-ckeditor/plugins'),
             ],
             'ckeditor'
         ); 
